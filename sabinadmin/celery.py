797a456b2d1422602d85b803023ea1d2025adb5a
yusif.h <yusif.h@vitanur.com>
2024-09-16 16:42:24 +0400

    initial

--- sabinadmin/celery.py
+++ sabinadmin/celery.py
@@ -8,6 +8,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sabinadmin.settings')
 
 app = Celery('sabinadmin')
 
+
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
